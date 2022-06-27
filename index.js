d58cee633de7cee037b3a640ab21dc2a7412c400
DayChoman <106967825+DayChoman@users.noreply.github.com>
2022-06-27 10:22:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+  //function ExibirNome (){
+    //let nome= document.getElementById ("campo-nome").value
+    // alert(nome)
+    //document.getElementById("cabeçalho").innerHTML = "Olá, " +nome + "!"
+ // }
+
+  function ExibirNúmero (){
+    let Númeral= document.getElementById ("Numero").value
+    if (Númeral% 2==0){
+        alert ("Numero Par")
+    } else {
+        alert ("Numero Ímpar")
+    }
+  }
